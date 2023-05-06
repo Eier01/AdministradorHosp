d539e36b51ff3f2da0b3a667607889973e20fac6
Sebastian Gutierrez <sebastiangutierrez1103@gmail.com>
2023-05-06 15:58:08 -0500

    feat: adjuntar de capacitaciones

--- CapaEntidad/CapacitacionesC.cs
+++ CapaEntidad/CapacitacionesC.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-    internal class CapacitacionesC
+    public class CapacitacionesC
     {
         public int idCapacitacionesCursos { get; set; }
         public string TipoFormacion { get; set; }
         public string Nombre  { get; set; }
         public string EstadoFormacion { get; set; }
-       
+        public string FechaInicio { get; set; }
+        public string FechaFinalizacion { get; set; }
+        public string Archivo { get; set; }
+
+        
+
+
+
 
 
     }
